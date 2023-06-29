8115debf0935091d658026fdb361d82031308896
serhat-coskun <serhatcoskun187424@gmail.com>
2023-06-29 20:17:41 +0300

    Extended blog app with posting, deleting, and updating posts, also added tests to cover new features

--- projects/simple-blog/django_project/blog/urls.py
+++ projects/simple-blog/django_project/blog/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
-from .views import PostListView, PostDetailView
+from .views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView,
+)
 
 urlpatterns = [
     path("", PostListView.as_view(), name="home"),
     path("post/<int:pk>", PostDetailView.as_view(), name="post_detail"),
+    path("post/new/", PostCreateView.as_view(), name="post_new"),
+    path("post/<int:pk>/update/", PostUpdateView.as_view(), name="post_update"),
+    path("post/<int:pk>/delete/", PostDeleteView.as_view(), name="post_delete"),
 ]
--- projects/simple-blog/django_project/blog/views.py
+++ projects/simple-blog/django_project/blog/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.urls import reverse_lazy
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
 from .models import Post
 
 # Create your views here.
@@ -13,3 +20,22 @@ class PostListView(ListView):
 class PostDetailView(DetailView):
     model = Post
     template_name = "post_detail.html"
+
+
+class PostCreateView(CreateView):
+    model = Post
+    template_name = "post_new.html"
+    fields = ["title", "author", "body"]
+
+
+class PostUpdateView(UpdateView):
+    model = Post
+    template_name = "post_update.html"
+    fields = ["title", "body"]
+
+
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = "post_delete.html"
+
+    success_url = reverse_lazy("home")
